2959e12e35b4ade5bd0f9d21fbf70e7e370b2704
DONGHYEON <dongheaey@gmail.com>
2022-11-27 02:50:11 +0900

    작동하는 임베 코드

---
+++ BMFO_LOCKER_SERVER.py
@@ -0,0 +1,36 @@
+#프로젝트명 
+from flask import Flask, request
+import RPi.GPIO as GPIO
+
+locker = [17, 27, 22, 10] #locker Id 는 Index - 1번이다
+
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+for i in locker :
+    GPIO.setup(i, GPIO.OUT, inital=GPIO.HIGH) #다 열려있어야함.
+
+
+@app.route("/open") #라커 문을 여는 API이다.
+def openLocker():
+    try:
+        params = request.get_json()
+        lockerId = params['lockerId']
+        GPIO.output(lockerId, GPIO.HIGH)
+        return True
+    except :
+        return False
+
+@app.route("/close") #라커 문을 닫는 API이다.
+def closeLocker():
+    try:
+        params = request.get_json()
+        lockerId = params['lockerId']
+        GPIO.output(locker[lockerId], GPIO.LOW)
+        return True
+    except:
+        return False
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ BMFO_QT_CLIENT.py
@@ -0,0 +1,131 @@
+import sys
+from PyQt5 import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import RPi.GPIO as GPIO
+import requests as req
+import simplejson
+from time import sleep
+
+led = [17, 27, 22, 10]
+url = "http://10.150.149.157/open"
+
+def set_GPIO():
+   GPIO.setmode(GPIO.BCM)
+   for item in led:
+      GPIO.setup(item, GPIO.OUT)
+
+def server(lckNum, password):
+   datas = {
+      'id' : lckNum,
+      'pass' : password
+   }
+   headers = {'Content-type': 'application/json', 'Accept': 'text/json'}
+   res = req.post(url, json=datas, headers=headers)
+   
+   print(res.json())
+   a = res.json()
+   if a['SUCCESS']==True:
+      print("opened")
+      GPIO.output(led[lckNum - 1], GPIO.HIGH)
+      sleep(1)
+      GPIO.output(led[lckNum - 1], GPIO.LOW)
+
+class Main(QWidget):
+    
+    switch_window = pyqtSignal(int)
+    
+    def __init__(self):
+        super().__init__()
+
+        main_layout = QGridLayout()
+        
+        button = {}
+        for i in range(0,4):
+            button[i] = QPushButtonNumber(str(i+1))
+            font = button[i].font()
+            font.setPointSize(40)
+            # button[i].setFont(font)
+            # button[i].setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            button[i].clicked.connect(lambda state, num = i+1 : self.number_button(num))
+            button[i].setStyleSheet("""
+                                    QPushButton{
+                                        color: white;
+                                        background-color: #00A5FF;
+                                        border: 0;
+                                        border-radius: 20px;
+                                    }
+                                    QPushButton:hover { background-color: #00b9ff; }
+                                    """)
+            
+            x, y = divmod(i, 2)
+            main_layout.addWidget(button[i], x, y)
+        
+        self.setLayout(main_layout)
+        self.resize(400, 400)
+        
+    def number_button(self, num):
+        self.switch_window.emit(num)
+        
+class Widget(QWidget):
+    
+    switch_window = pyqtSignal(int)
+    
+    def __init__(self, number):
+        super().__init__()
+
+        main_layout = QFormLayout()
+        
+        self.password = QLineEdit(self)
+        self.password.setPlaceholderText("Set your password")
+        self.password.setEchoMode(QLineEdit.Password);
+        
+        check_button = QPushButton("check")
+        check_button.clicked.connect(lambda state, num = number : self.check_button(number))
+        
+        main_layout.addRow("password: ", self.password)
+        main_layout.addRow(check_button)
+        
+        self.setLayout(main_layout)
+        self.resize(400, 400)
+        
+    def check_button(self, num):
+        password = self.password.text()
+        self.password.setText("")
+        if(password != ""):
+            print(num, password)
+            set_GPIO()
+            server(num, password)
+
+class QPushButtonNumber(QPushButton):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        font = QFont("Helvetia", 20)
+        font.setBold(True)
+        self.setFont(font)
+        
+        
+class Controller:
+    def __init__(self):
+        pass
+    
+    def show_main(self):
+        self.main = Main()
+        self.main.switch_window.connect(self.show_widget)
+        self.main.show()
+        
+    def show_widget(self, number):
+        self.widget = Widget(number)
+        self.widget.show()
+        
+def main_app():
+    app = QApplication(sys.argv)
+    controller = Controller()
+    controller.show_main()
+    sys.exit(app.exec_())
+        
+
+if __name__ == '__main__':
+    main_app()
